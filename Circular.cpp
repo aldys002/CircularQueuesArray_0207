361c2ca0337a86fd12c188890e5b0b63c1977374
aldys002 <igidiaaldys@gmail.com>
2024-05-30 14:48:33 +0700

    membuat fungsi if untuk mengecek apakah antrian penuh

--- Circular.cpp
+++ Circular.cpp
@@ -16,5 +16,11 @@ public:
     int num;
     cout << "Enter a number: ";
     cin >> num;
+    cout << endl;
+
+    // cek apakah antrian penuh
+    if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+        
+    }
    }
 };
