bf93987c24a0bdd910685aeea9f42beb3ee59010
aldys002 <igidiaaldys@gmail.com>
2024-05-30 15:41:25 +0700

    default

--- Circular.cpp
+++ Circular.cpp
@@ -132,6 +132,10 @@ int main() {
             case '4' : {
                 return 0;
             }
+            default: {
+                cout << "Invalid option!" << endl;
+                break;
+            }
         }
     }
    } 
